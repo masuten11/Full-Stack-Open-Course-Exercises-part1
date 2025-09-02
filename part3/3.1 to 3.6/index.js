35c5f08843e048e24102ef1b15083e26761ab170
masuten11 <masuwillriseagain2023@gmail.com>
2025-09-02 15:40:14 +0600

    3.3

--- part3/3.1 to 3.6/index.js
+++ part3/3.1 to 3.6/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-
 const app = express();
 
 const phonebook = [
@@ -10,10 +9,35 @@ const phonebook = [
   { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
 
-// Route for /api/persons/
+// Route for /api/persons
 app.get('/api/persons', (req, res) => {
   res.json(phonebook);
-})
+});
+
+// Route for /info
+app.get('/info/', (req, res) => {
+  const persons = phonebook.length;
+  const currTime = new Date();
+  const message =
+  `<p>Phonebook has info for ${persons} people</p>
+   <p>${currTime}</p>`;
+
+   // Send the response
+   res.send(message);
+});
+
+// Route for /api/persons/:id
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = phonebook.find(person => person.id === id);
+
+  // response
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  };
+}); 
 
 const PORT = 3001;
 app.listen(PORT, () => {
