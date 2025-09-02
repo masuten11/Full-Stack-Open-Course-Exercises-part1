1a904f6dbf7f52a137ab298da12b326abc80bce3
masuten11 <masuwillriseagain2023@gmail.com>
2025-09-02 15:04:28 +0600

    3.1

---
+++ part3/3.1 to 3.6/index.js
@@ -0,0 +1,34 @@
+import http from 'http';
+
+const phonebook = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type" : "application/json"});
+  res.end(JSON.stringify(phonebook));
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
